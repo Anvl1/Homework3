620b530fc9a581188df8a6d61ec1b956f5746dba
Anvl1 <88466348+Anvl1@users.noreply.github.com>
2025-05-18 21:19:02 +1000

    Add files via upload

---
+++ Homework3.1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+ setlocale(LC_ALL, "");
+cout << "Оператор ||:" << endl;
+
+boolalpha(cout);
+    bool a ;
+    bool b ;
+   
+    a = true;
+    b = true;
+    cout << " " << a << " " << b << " " << (a || b) << endl;
+
+    a = false;
+    b = true;
+    cout << " " << a << " " << b << " " << (a || b) << endl;
+
+    a = true;
+    b = false;
+    cout << " " << a << " " << b << " " << (a || b) << endl;
+
+    a = false;
+    b = false;
+    cout << " " << a << " " << b << " " << (a || b) <<  endl;
+
+cout << "Оператор &&:" << endl;
+   
+    a = true;
+    b = true;
+    cout << " " << a << " " << b << " " << (a && b) << endl;
+
+    a = false;
+    b = true;
+    cout << " " << a << " " << b << " " << (a && b) << endl;
+
+    a = true;
+    b = false;
+    cout << " " << a << " " << b << " " << (a && b) << endl;
+
+    a = false;
+    b = false;
+    cout << " " << a << " " << b << " " << (a && b) << endl;
+}
