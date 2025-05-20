ea5308626caa9a8bbcd34f479969feb5d64139c6
Anvl1 <88466348+Anvl1@users.noreply.github.com>
2025-05-20 13:44:30 +1000

    Add files via upload

---
+++ Homework3.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>﻿
+using namespace std;
+
+int main()
+{
+	int num1, num2, num3;
+
+	setlocale(LC_ALL, "");
+	wcout << L"Введите первое число: " << endl;
+	cin >> num1;
+	wcout << L"Введите второе число: " << endl;
+	cin >> num2;
+	wcout << L"Введите третье число: " << endl;
+	cin >> num3;
+
+	int max = (num1 > num2) ? ((num1 > num3) ? num1 : num3) : ((num2 > num3) ? num2 : num3);
+	int min = (num1 < num2) ? ((num1 < num3) ? num1 : num3) : ((num2 < num3) ? num2 : num3);
+	int mid = (num1 + num2 + num3) - max - min;
+
+	std:: wcout << L"Результат: " << max << ", " << mid << ", " << min << endl;
+
+	return 0;
+}
