6bd111a8c4a1e96e844d1d9b4cee7e98864eed05
Anvl1 <88466348+Anvl1@users.noreply.github.com>
2025-05-22 23:17:03 +1000

    Add files via upload

--- Homework3.1.cpp
+++ Homework3.1.cpp
@@ -2,47 +2,47 @@
 #include <string>
 
 using namespace std;
-
 int main() 
+
 {
- setlocale(LC_ALL, "");
- cout << "Оператор ||:" << endl;
+	setlocale(LC_ALL, "");
+	cout << "Оператор ||:" << endl;
 
- boolalpha(cout);
- bool a;
- bool b;
+	boolalpha(cout);
+	bool a;
+	bool b;
    
-  a = true;
-  b = true;
-  cout << " " << a << " " << b << " " << (a || b) << endl;
+	a = true;
+	b = true;
+	cout << " " << a << " " << b << " " << (a || b) << endl;
 
-  a = false;
-  b = true;
-  cout << " " << a << " " << b << " " << (a || b) << endl;
+	a = false;
+	b = true;
+	cout << " " << a << " " << b << " " << (a || b) << endl;
 
-  a = true;
-  b = false;
-  cout << " " << a << " " << b << " " << (a || b) << endl;
+	a = true;
+	b = false;
+	cout << " " << a << " " << b << " " << (a || b) << endl;
+	
+	a = false;
+	b = false;
+	cout << " " << a << " " << b << " " << (a || b) <<  endl;
 
-  a = false;
-  b = false;
-  cout << " " << a << " " << b << " " << (a || b) <<  endl;
-
-  cout << "Оператор &&:" << endl;
+	cout << "Оператор &&:" << endl;
    
-  a = true;
-  b = true;
-  cout << " " << a << " " << b << " " << (a && b) << endl;
+	a = true;
+	b = true;
+	cout << " " << a << " " << b << " " << (a && b) << endl;
 
-  a = false;
-  b = true;
-  cout << " " << a << " " << b << " " << (a && b) << endl;
+	a = false;
+	b = true;
+	cout << " " << a << " " << b << " " << (a && b) << endl;
 
-  a = true;
-  b = false;
-  cout << " " << a << " " << b << " " << (a && b) << endl;
-
-  a = false;
-  b = false;
-  cout << " " << a << " " << b << " " << (a && b) << endl;
+	a = true;
+	b = false;
+	cout << " " << a << " " << b << " " << (a && b) << endl;
+	
+	a = false;
+	b = false;
+	cout << " " << a << " " << b << " " << (a && b) << endl;
 }
